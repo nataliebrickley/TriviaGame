8881996337664e2295075c17a047ce2523594421
Natalie Brickley <natt_rose@yahoo.com>
2019-02-28 08:39:16 -0800

    Added timer and set new variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,13 +5,20 @@ var quiz = [
     {q: "Question 4", a: "choice 1", b: "choice2", c: "choice3", answer: "answer4"},
     {q: "Question 5", a: "choice 1", b: "choice2", c: "choice3", answer: "answer5"}
 ]
-
+var timer = 60;
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0; 
 //when the start button is clicked...
 $("#start").click(function(){
 //hide the start button
     $("#start").hide();
-//...show timer
-
+//...show and start the timer
+    $("#timer").html("<p>Time Remaining: " + timer + " seconds</p>")
+    var intervalId = setInterval(function(){
+        timer--;
+        $("#timer").html("<p>Time Remaining: " + timer + " seconds</p>")
+    }, 1000);
 //...show questions and options
     for (var i = 0; i<quiz.length; i++) {
        // $("#quiz").append("<p>" + quiz[i].q + "</p>")
@@ -21,4 +28,6 @@ $("#start").click(function(){
        var choice3 = '<label class="radio-inline"><input type="radio" name=' + i + "'>"+quiz[i].c + '</label>'
        $("#quiz").append("<div>" + question + choice1 + choice2 + choice3 + "</div>")
     }
+//..show the done button
+    $("#quiz").append("<button id='done'>Done</button>");
 })
