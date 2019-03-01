6ce1d9a2efe6f4db412eb3a13d0576c096ebda25
Natalie Brickley <natt_rose@yahoo.com>
2019-02-28 17:50:16 -0800

    Added code for when done button is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,43 @@
 var quiz = [
-    {q: "Question 1", a: "choice 1", b: "choice2", c: "choice3", answer: "answer1"},
-    {q: "Question 2", a: "choice 1", b: "choice2", c: "choice3", answer: "answer2"},
-    {q: "Question 3", a: "choice 1", b: "choice2", c: "choice3", answer: "answer3"},
-    {q: "Question 4", a: "choice 1", b: "choice2", c: "choice3", answer: "answer4"},
-    {q: "Question 5", a: "choice 1", b: "choice2", c: "choice3", answer: "answer5"}
+    { q: "Question 1", a: "choice 1", b: "choice2", c: "choice3", answer: "answer1" },
+    { q: "Question 2", a: "choice 1", b: "choice2", c: "choice3", answer: "answer2" },
+    { q: "Question 3", a: "choice 1", b: "choice2", c: "choice3", answer: "answer3" },
+    { q: "Question 4", a: "choice 1", b: "choice2", c: "choice3", answer: "answer4" },
+    { q: "Question 5", a: "choice 1", b: "choice2", c: "choice3", answer: "answer5" }
 ]
 var timer = 60;
 var correct = 0;
 var incorrect = 0;
-var unanswered = 0; 
+var unanswered = 0;
 //when the start button is clicked...
-$("#start").click(function(){
-//hide the start button
+$("#start").click(function () {
+    //hide the start button
     $("#start").hide();
-//...show and start the timer
+    //...show and start the timer
     $("#timer").html("<p>Time Remaining: " + timer + " seconds</p>")
-    var intervalId = setInterval(function(){
+    var intervalId = setInterval(function () {
         timer--;
         $("#timer").html("<p>Time Remaining: " + timer + " seconds</p>")
     }, 1000);
-//...show questions and options
-    for (var i = 0; i<quiz.length; i++) {
-       // $("#quiz").append("<p>" + quiz[i].q + "</p>")
-       var question = "<p>" + quiz[i].q + "</p>"
-       var choice1 = '<label class="radio-inline"><input type="radio" name=' + i + "'>"+quiz[i].a + '</label>'
-       var choice2 = '<label class="radio-inline"><input type="radio" name=' + i + "'>"+quiz[i].b + '</label>'
-       var choice3 = '<label class="radio-inline"><input type="radio" name=' + i + "'>"+quiz[i].c + '</label>'
-       $("#quiz").append("<div>" + question + choice1 + choice2 + choice3 + "</div>")
+    //...show questions and options
+    for (var i = 0; i < quiz.length; i++) {
+        // $("#quiz").append("<p>" + quiz[i].q + "</p>")
+        var question = "<p>" + quiz[i].q + "</p>"
+        var choice1 = '<label class="radio-inline"><input type="radio" name=' + i + "'>" + quiz[i].a + '</label>'
+        var choice2 = '<label class="radio-inline"><input type="radio" name=' + i + "'>" + quiz[i].b + '</label>'
+        var choice3 = '<label class="radio-inline"><input type="radio" name=' + i + "'>" + quiz[i].c + '</label>'
+        $("#quiz").append("<div>" + question + choice1 + choice2 + choice3 + "</div>")
     }
-//..show the done button
+    //..show the done button
     $("#quiz").append("<button id='done'>Done</button>");
+
+    //when the done button is clicked...
+    $("#done").click(function () {
+        //...hide the quiz and timer
+        $("#quiz").hide();
+        $("#timer").hide();
+        //...and show the results
+        $("#results").html("<p>All Done!</p><p>Correct Answers: " + correct + "</p><p>Incorrect Answers: " + incorrect + "</p><p>Unanswered Questions: " + unanswered + "</p>")
+    })
 })
+
